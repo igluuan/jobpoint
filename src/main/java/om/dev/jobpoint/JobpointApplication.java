6b9659453dabde3067b93fa097ff52e4ddb78238
helpdesk - pc <contatoluansantos7@gmail.com>
2025-08-13 15:35:15 -0300

    feat: iniciando projeto.

---
+++ src/main/java/om/dev/jobpoint/JobpointApplication.java
@@ -0,0 +1,13 @@
+package om.dev.jobpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/om/dev/jobpoint/JobpointApplicationTests.java
@@ -0,0 +1,13 @@
+package om.dev.jobpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
